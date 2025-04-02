fecbfeb96514dc4c315eec112072f8080dc54037
Muml3ys4war <muml3ys4@gmail.com>
2025-04-02 20:48:47 +0530

    refactor: commands for clarity

--- apache/100_apache-system-logging/foreground.sh
+++ apache/100_apache-system-logging/foreground.sh
@@ -1,2 +1,3 @@
-echo "Initializing Lab......"
-sleep 100
+echo "Installing scenario..."
+while [ ! -f /tmp/finished ]; do sleep 1; done
+echo DONE
--- apache/100_apache-system-logging/setup.sh
+++ apache/100_apache-system-logging/setup.sh
@@ -1,4 +1,6 @@
 apt-get install apache2 -y
 systemctl start apache2
 cd /etc/apache2/site-available; a2dissite 000-default.conf
-cd
+cd 
+
+touch /tmp/finished
