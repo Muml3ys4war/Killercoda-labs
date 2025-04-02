82362e9dde129da6b741f3311cb6701040129374
Muml3ys4war <muml3ys4@gmail.com>
2025-04-02 20:58:03 +0530

    fix : default conf path

--- apache/100_apache-system-logging/setup.sh
+++ apache/100_apache-system-logging/setup.sh
@@ -1,6 +1,7 @@
 apt-get install apache2 -y
 systemctl start apache2
-cd /etc/apache2/site-available; a2dissite 000-default.conf
+cd /etc/apache2/sites-available; 
+a2dissite 000-default.conf
 cd 
 
 touch /tmp/finished
